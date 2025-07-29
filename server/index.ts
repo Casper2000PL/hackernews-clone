f18760819d505cb0a56e907813fe6fd1f0a439d0
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-07-29 22:02:08 +0200

    2. Drizzle Setup and Hono Error Handler

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,43 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+import type { ErrorResponse } from "@/shared/types";
 
 const app = new Hono();
 
 app.get("/", (c) => {
+  throw new HTTPException(404, { message: "404 NOT FOUND" });
+
   return c.text("Hello Hono!");
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    const errorResponse =
+      err.res ??
+      c.json<ErrorResponse>({
+        success: false,
+        error: err.message,
+        isFormError:
+          err.cause && typeof err.cause === "object" && "form" in err.cause
+            ? err.cause.form === true
+            : false,
+      });
+
+    return errorResponse;
+  }
+
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      error:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : (err.stack ?? err.message),
+      isFormError: false,
+    },
+    500,
+  );
 });
 
 export default app;
