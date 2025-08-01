a0f9317c491a9c73511c37d76d51894ac794d761
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-08-01 23:52:51 +0200

    7. Frontend setup

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+
+export const Route = createFileRoute("/")({
+  component: HomeComponent,
+});
+
+function HomeComponent() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+      <div className="bg-black text-red-500">asasd</div>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
